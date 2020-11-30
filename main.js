74e2a052a9420cf7ebdce442c4bab8f072803239
blacc-snow68 <75212615+blacc-snow68@users.noreply.github.com>
2020-11-30 12:42:19 +0100

    Add files via upload
    My first project....... Just menus and a little javascript

---
+++ main.js
@@ -0,0 +1,19 @@
+function openNav() {
+  document.getElementById("aside").style.height = "270px";
+  document.getElementById("list").style.display = "block";
+} 
+
+function closeNav() {
+  document.getElementById("aside").style.height = "0";
+  document.getElementById("list").style.display = "none";
+}
+  
+function openNavv() {
+ document.getElementById("league").style.height = "540px";
+ document.getElementById("list2").style.display = "block";
+}
+
+function closeNavv() {
+ document.getElementById("league").style.height = "0";
+ document.getElementById("list2").style.display = "none";
+}
